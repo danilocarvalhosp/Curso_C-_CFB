10cf7878cbfc52f1fb1eb7afe173ea8a9be62239
Danilo Carvalho <danilocarvalhosp@gmail.com>
2021-12-10 20:00:29 -0300

    Aula 02
    Criando executável C# .Net

---
+++ Aula02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFB Cursos");
+            if(args.GetLength(0) > 0){
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
+
+// Criando programa C# .NET - no terminal, dentro da pasta do arquivo, digitar "dotnet new console"
