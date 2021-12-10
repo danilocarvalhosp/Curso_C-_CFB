960d6edf71a73e80b5a34227e6f1bb982beabccb
Danilo Carvalho <danilocarvalhosp@gmail.com>
2021-12-10 19:43:46 -0300

    Aula 01
    Introdução ao Curso de C#

---
+++ Aula01/aula01.cs
@@ -0,0 +1,10 @@
+using System;
+class Principal{
+    static void Main(){
+        Console.Write("Olá Mundo...");
+    }
+}
+
+// Criando EXE = dentro do diretório, digitar "CSC NomeDoArquivo"
+
+// Executando EXE = dentro do diretório, digitar "NomeDoArquivo"
