6af649dc0e12eea5deeab5867fc467bf5122533f
Danilo Carvalho <danilocarvalhosp@gmail.com>
2021-12-11 10:13:06 -0300

    Aula 07
    Constantes

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Aula07{
+    static void Main(){
+        const string canal = "CFB Cursos";
+        const double pi = 3.1415;
+
+        System.Console.WriteLine("Canal {0}", canal);
+    }
+}
