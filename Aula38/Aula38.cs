1d8ee9fa9841b6ec53ee38bf29413ec93b3795b7
Danilo Carvalho <danilocarvalhosp@gmail.com>
2021-12-14 19:48:08 -0300

    Aula 38
    Métodos virtuais

---
+++ Aula38/Aula38.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Base{
+    public Base(){
+        System.Console.WriteLine("Construtor da classe Base");
+    }
+
+    virtual public void info(){}
+}
+
+class Derivada1 : Base {
+    public Derivada1(){
+        System.Console.WriteLine("Construtor da classe Derivada 1");
+    }
+
+    override public void info(){
+        System.Console.WriteLine("Derivada 1");
+    }
+
+}
+
+class Derivada2 : Derivada1 {
+    public Derivada2(){
+        System.Console.WriteLine("Construtor da classe Derivada 2");
+    }
+
+    override public void info(){
+        System.Console.WriteLine("Derivada 2");
+    }
+
+}
+
+class Aula38{
+    static void Main(){
+        Base Ref;
+        Derivada1 derivada1 = new Derivada1();
+        Derivada2 derivada2 = new Derivada2();
+
+        Ref = derivada2;
+        Ref.info();
+    }
+}
